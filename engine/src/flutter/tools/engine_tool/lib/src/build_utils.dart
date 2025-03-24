a39a23e68b0d3893f03ff8d6584c229d03d89764
Jesse Ezell <jesse.ezell@timu.com>
2025-03-23 17:08:45 -0700

    don't run tests

--- engine/src/flutter/tools/engine_tool/lib/src/build_utils.dart
+++ engine/src/flutter/tools/engine_tool/lib/src/build_utils.dart
@@ -97,7 +97,7 @@ Future<int> runBuild(
     rbeConfig: rbeConfig,
     concurrency: concurrency,
     extraGnArgs: gnArgs,
-    runTests: true,
+    runTests: false,
     extraNinjaArgs: <String>[
       ...targets.map((Label label) => label.toNinjaLabel()),
       // If the environment is verbose, pass the verbose flag to ninja.
